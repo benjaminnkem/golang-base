bff3329b4a7a72afdb4d83a32bc10e9b7e074541
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-01-29 20:35:50 +0100

    chore: first try of Golang

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func somethingElse() {
+	fmt.Println("this is ran from the 'something else' function.")
+}
+
+func greet(name string) {
+	fmt.Println("Hello,", name)
+}
+
+func calculateCircumference(radius float64) float64 {
+	return 2 * math.Pi * radius
+}
+
+func main() {
+
+	var firstName string = "Benjamin"
+	lastName := "Nkem"
+
+	fmt.Println(firstName, lastName)
+
+	var age int8 = 100
+	fmt.Println(age)
+
+	gpa := 0.5
+	fmt.Println(gpa)
+
+	somethingElse()
+	greet("Benjamin Nkem")
+
+	result := calculateCircumference(30)
+	fmt.Println(result)
+}
