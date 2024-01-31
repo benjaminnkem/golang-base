9572c2895b4553cfcf5427a01e5deb77306678d3
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-01-31 22:53:11 +0100

    learned about functions

--- main.go
+++ main.go
@@ -1,28 +1,81 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func sayGreeting(name string) {
+	fmt.Println("Hi,", strings.ToUpper(name))
+}
+
+func sayBye(name string) {
+	fmt.Println("Goodbye,", name)
+}
+
+func cycleNames(names []string, fn func(string)) {
+	for _, name := range names {
+		fn(name)
+	}
+}
+
+func circleArea(r float64) float64 {
+	return math.Pi * math.Pow(r, 2)
+}
+
+func calculateCircumference(radius float64) float64 {
+	return 2 * math.Pi * float64(radius)
+}
+
+func findName(names []string, target string) string {
+	for _, v := range names {
+		if v == target {
+			return "target found"
+		}
+	}
+
+	return "not found"
+}
+
+func getInitials(name string) (string, string) {
+
+	s := strings.ToUpper(name)
+	names := strings.Split(s, " ")
+
+	var initials []string
+
+	for _, v := range names {
+		initials = append(initials, v[:1])
+	}
+
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+
+	return initials[0], ""
+
+}
 
 func main() {
 
-	// i := 0
+	names := []string{"benjamin", "michael", "collins", "king david", "francisca"}
 
-	// for i < 50 {
-	// 	fmt.Println(i)
-	// 	i++
-	// }
+	// cycleNames(names, sayGreeting)
 
-	// for i := 1; i <= 50; i++ {
-	// 	fmt.Println(i)
-	// }
+	fmt.Printf("The circumference is: %0.3f to 3.d.p\n", calculateCircumference(26.5))
 
-	names := []string{"benjamin", "tochi", "junior", "nkem"}
+	// c1 := circleArea(26.5)
+	// c2 := circleArea(40)
 
-	// for i := 0; i < len(names); i++ {
-	// 	fmt.Println(names[i])
-	// }
+	// fmt.Printf("The area is: %0.3f to 3.d.p\n", c1)
+	// fmt.Printf("The area is: %0.3f to 3.d.p\n", c2)
 
-	for _, value := range names {
-		fmt.Printf("this is the value %s\n", value)
-	}
+	result := findName(names, "collins")
 
+	fmt.Println(result)
+
+	name1, name2 := getInitials("benjamin nkem")
+
+	fmt.Println(name1, name2)
 }
