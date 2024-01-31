0ebd6179388087f3afb67c7e71ba3c51668c3d6a
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-01-31 22:02:19 +0100

    index, value in for loops

--- main.go
+++ main.go
@@ -17,12 +17,12 @@ func main() {
 
 	names := []string{"benjamin", "tochi", "junior", "nkem"}
 
-	for i := 0; i < len(names); i++ {
-		fmt.Println(names[i])
-	}
-
-	// for index, value range names {
-	// 	fmt.Println("the index is %v and value is %v", index, value)
+	// for i := 0; i < len(names); i++ {
+	// 	fmt.Println(names[i])
 	// }
+
+	for _, value := range names {
+		fmt.Printf("this is the value %s\n", value)
+	}
 
 }
