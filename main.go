8ad583f8548af794b35c95887a78f17f5189d0f7
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-02-01 20:43:56 +0100

    learned about pointers and pointer-based values

--- main.go
+++ main.go
@@ -2,16 +2,30 @@ package main
 
 import "fmt"
 
-func updateName(n string) {
-	n = "benjamin nkem"
+func updateName(n *string) {
+	*n = "benjamin"
+}
+
+func updateNames(n []string) {
+	n[0] = "maleek"
 }
 
 func main() {
 
 	name := "nkem"
 
-	updateName(name)
+	m := &name
+
+	updateName(m)
+
+	// fmt.Println("variable 'name' memory location is:", m)
+	// fmt.Printf("variable 'name' value at location %v is: %v\n", m, *m)
+
+	names := []string{"benjamin", "collins", "david", "michael", "francisca"}
+
+	updateNames(names)
 
 	fmt.Println(name)
+	fmt.Println(names)
 
 }
