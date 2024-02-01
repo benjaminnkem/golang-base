9986ea726b5714b28ab61e159d429d0fbe4fa391
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-02-02 00:19:00 +0100

    learned about defining custom types/structs

--- main.go
+++ main.go
@@ -2,30 +2,9 @@ package main
 
 import "fmt"
 
-func updateName(n *string) {
-	*n = "benjamin"
-}
-
-func updateNames(n []string) {
-	n[0] = "maleek"
-}
-
 func main() {
 
-	name := "nkem"
-
-	m := &name
-
-	updateName(m)
-
-	// fmt.Println("variable 'name' memory location is:", m)
-	// fmt.Printf("variable 'name' value at location %v is: %v\n", m, *m)
-
-	names := []string{"benjamin", "collins", "david", "michael", "francisca"}
-
-	updateNames(names)
-
-	fmt.Println(name)
-	fmt.Println(names)
+	myBill := newBill("benjamin")
+	fmt.Println(myBill)
 
 }
